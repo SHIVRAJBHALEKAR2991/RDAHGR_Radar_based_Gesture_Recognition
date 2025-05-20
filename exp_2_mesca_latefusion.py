d3c1a47d4084684ada741372b7e1ffce79b0c6cf
Shivraj Bhalekar <shivrajbhalekar2991@gmail.com>
2025-05-20 17:24:40 +0530

    epochs reduced to 29

--- exp_2_mesca_latefusion.py
+++ exp_2_mesca_latefusion.py
@@ -740,7 +740,7 @@ model.summary()
 ###### Training the Model
 history = model.fit(
     [X_train_rdi, X_train_rai,y_train_onehot], y_train_onehot,
-    epochs=30,
+    epochs=29,
     batch_size=2,
     validation_data=([X_dev_rdi, X_dev_rai,y_dev_onehot], y_dev_onehot),
     validation_batch_size=2
